3c8650db7d91757e8879ad6527264bb56af744aa
datpham2 <0306181218@caothang.edu.vn>
2022-04-09 17:19:49 +0700

    First Commit. Add a list of music to the home page.

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Music</title>
+    </head>
+    <body>
+        <ul>
+            <li>비긴어게인 오픈마이크 EPISODE. 33</li>
+            <li>i</li>
+        </ul>
+    </body>
+</html>
